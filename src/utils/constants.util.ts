a83e700b6b7080b87872c5543ec1045e47a49374
Andi Ashari <a.ashari1302@gmail.com>
2025-05-01 15:53:06 +0700

    fix: manually set version to 1.18.2
    Manually updated version using update:version script to resolve semantic-release tag conflict.

--- src/utils/constants.util.ts
+++ src/utils/constants.util.ts
@@ -9,7 +9,7 @@
  * Current application version
  * This should match the version in package.json
  */
-export const VERSION = '1.14.2';
+export const VERSION = '1.18.2';
 
 /**
  * Package name with scope
